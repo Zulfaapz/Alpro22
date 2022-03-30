d8025cedfc228a6b28307a608dcf9d1d9084dbcc
Zulfaa Putri Az Zahra <101779450+Zulfaapz@users.noreply.github.com>
2022-03-30 11:27:55 +0700

    studi kasus 2

---
+++ studikasus2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+	class restoran {
+	friend ostream&operator<<(ostream&, const restoran&);
+	friend istream& operator>>(istream&, restoran&);
+	public:
+restoran ();
+	void jumlah, total, harga, jarak, menu, ongkir, diskon, bayar, subtotal;
+	private:
+	int a, b, c, d, e, f, g, h;
+	int subtotal;
+};
