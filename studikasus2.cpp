955446a515988ac5295f0c26d21ddddf6ca39c09
BungaSyahiraNajla <101790324+BungaSyahiraNajla@users.noreply.github.com>
2022-03-30 11:44:22 +0700

    output

--- studikasus2.cpp
+++ studikasus2.cpp
@@ -16,3 +16,28 @@ restoran::restoran(){
 	cout<<"               PROGRAM RUMAH MAKAN DELIVERY                "<<endl;
 	cout<<"==========================================================="<<endl;
 	
+	
+//output
+	cout<<"====================================\n";
+		
+		cout<<"struk akan di cetak\n";
+		ofstream kasir;
+		kasir.open("strukblnjaa.txt", ios::app);
+		kasir<<"==========================\n";
+		kasir<<"STRUK BELANJA\n";
+		kasir<<"==========================\n";
+		for (int i=0; i<a; i++){
+			ksr[1].total = ksr[i].kuantitas * ksr[i].harga;
+			kasir<<ksr[i].nama_barang<<endl;
+			kasir<<ksr[i].kuantitas<<"X"<<ksr[i].harga<<"="<<ksr[1].total<<endl;
+			ksr[1].total += ksr[i].total;
+		}
+		kasir<<"=========================\n";
+		kasir<<"Total belanja : "<<ksr[1].total;
+		kasir<<"Terimakasih telah Berbelanja\n";
+		kasir<<"=========================\n";
+		kasir<<"\n";
+		kasir.close();
+	}
+}
+	
